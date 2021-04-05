f94a45042b53b1633e459818e6b6ecf3ef0d9402
mattgr99 <jmgranja1@espe.edu.ec>
2021-04-05 15:15:23 -0500

    Autenticacion agregada

--- ApiGateway1/src/main/java/com/escolastico/proy/ApiGateway1Application.java
+++ ApiGateway1/src/main/java/com/escolastico/proy/ApiGateway1Application.java
@@ -3,8 +3,10 @@ package com.escolastico.proy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 @EnableZuulProxy
 public class ApiGateway1Application {
 
